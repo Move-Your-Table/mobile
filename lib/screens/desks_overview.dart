cee2bc4effdb3df7e11282c7178e1c9e5bdb9f1c
steffen.gemin <steffen.gemin@student.howest.be>
2021-12-12 09:58:25 +0100

    Add file for desks overview

---
+++ lib/screens/desks_overview.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DeskOverview extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
