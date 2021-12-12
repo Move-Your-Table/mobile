0f122d83afac6d2cc53b9e021b91bfb6815eb7e9
steffen.gemin <steffen.gemin@student.howest.be>
2021-12-12 10:47:06 +0100

    Add card for desks

--- lib/screens/desks_overview.dart
+++ lib/screens/desks_overview.dart
@@ -7,7 +7,88 @@ class DesksOverview extends StatelessWidget {
         backgroundColor: const Color(0xFF161A20),
         body: SafeArea(
           child: Column(
-            children: [_roomCard()],
+            children: [_roomCard(), _deskCard()],
+          ),
+        ));
+  }
+
+  Widget _deskCard() {
+    return Padding(
+        padding: const EdgeInsetsDirectional.fromSTEB(28, 15, 28, 15),
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xFF1E222D),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(0),
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(0),
+                ),
+                child: Image.network(
+                  'https://picsum.photos/seed/586/600',
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(17, 0, 0, 0),
+                child: Container(
+                  height: 100,
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Desk 666',
+                        style: TextStyle(
+                          fontFamily: 'Nunito',
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 5, 0, 0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Container(
+                              width: 100,
+                              height: 20,
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF22E95E),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              alignment: const AlignmentDirectional(0, 0),
+                              child: const Text(
+                                'Available',
+                                style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  color: Color(0xFF161A20),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ));
   }
